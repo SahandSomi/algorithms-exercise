86277e40328840e95318844694b70681b95d1922
Sahand <ssomi@ualberta.ca>
2023-08-23 22:46:49 -0600

    Trapping_rain_water_part_2

--- Sliding Window/Trapping_rain_water/Trapping_rain_water.py
+++ Sliding Window/Trapping_rain_water/Trapping_rain_water.py
@@ -1,25 +1,33 @@
 import pytest
+from heapq import heapify, heappush, heappop
 
 class Solution(object):
 
     def Trapping_rain_water_SS(self, numbers):
 
-        left, right = 0, 1
         result = 0
+        heap_left = heapq.heapify([0])
+        heap_left = heapq.heapify(numbers)
 
-        while left == len(numbers) and right == len(numbers):
+        for idx in range(0,len(numbers)-1):
+
+            if len(numbers[:idx]) == 0:
+                left_min = 0
+
+            else:
+                left_min = max(numbers[:idx])
+
+            if len(numbers[idx+1:]) == 0:
+                right_min = 0
+
+            else:
+                right_min = max(numbers[idx+1:])
+
+            if  min(left_min, right_min) - numbers[idx] > 0 :
+                result += min(left_min, right_min) - numbers[idx]
 
 
-            if numbers[left] == 0:
-                left += 1
-
-            if numbers[right] == 0:
-                right += 1
-
-            if numbers[right] >= numbers[left]:
-                right = left
-                left += 1
-
+        return result
 
             
     
