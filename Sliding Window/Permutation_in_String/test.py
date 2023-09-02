2c7ae1c0b0ceac3eaae2f7d6dd34c21a1a47a524
Sahand <ssomi@ualberta.ca>
2023-09-01 23:47:44 -0600

    Permutation_in_String_neat code

--- Sliding Window/Permutation_in_String/test.py
+++ Sliding Window/Permutation_in_String/test.py
@@ -6,4 +6,4 @@ import pytest
 @pytest.mark.parametrize("expected,test_input", [(True,["ab","eidbaooo"]), (False,["ab","eidboaoo"])])
 def test_1(expected,test_input):
     answer = Solution()
-    assert answer.Permutation_in_String_SS(test_input[0],test_input[1]) == expected
+    assert answer.Permutation_in_String_neat(test_input[0],test_input[1]) == expected
